5adef7e220cc34353e9db280841dd48fb690710b
Nicolás Nieto <niconieto1506@gmail.com>
2021-04-26 16:13:24 -0500

    Corrección ruta login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '/login', component: LoginComponent}
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
