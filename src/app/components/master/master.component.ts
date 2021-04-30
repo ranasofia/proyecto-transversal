100eb2ffe6d99732c855f6656b7e8967c8c4e1ff
1Alejandra3 <palejandraguzman@ucundinamarca.edu.co>
2021-04-29 20:24:51 -0500

    Documentacion componente master

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -2,30 +2,55 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from './../../../environments/environment';
 import { ClienteService } from 'src/app/_service/cliente.service';
 import { Component, OnInit , DoCheck} from '@angular/core';
-
+/**
+ * Decorador de Mastercomponent
+ */
 @Component({
   selector: 'app-master',
   templateUrl: './master.component.html',
   styleUrls: ['./master.component.css']
 })
+/**
+ * Clase de Mastercomponent
+ */
 export class MasterComponent implements OnInit , DoCheck{
+  /**
+   * Variable que alamacena el token que se genera al ingresar al login
+   */
   token: string;
+  /**
+   * Constructor que inicializa las variables globales de componente
+   * @param clienteService 
+   */
   constructor(private clienteService:ClienteService) { }
 
   ngOnInit(): void {
     
   }
 
+  /**
+   * Implementacion que se ejecuta cada vez que se verifican las propiedades de entrada del componente
+   */
   ngDoCheck() : void{
-    console.log("funcionó");
+   
     this.token = sessionStorage.getItem(environment.TOKEN);
   }
 
+  /**
+   * Metodo que se encarga de cerrar sesion y eliminar el token
+   */
   cerrarSesion(){
+    /**
+     * Constante para decodificar el token
+     */
     const HELPER = new JwtHelperService();
+    /**
+     * Variable que decodifica el toquen y captura el usuario logueado
+     */
     var nombre = HELPER.decodeToken(sessionStorage.getItem(environment.TOKEN))["name"];
     this.clienteService.putCerrarSesion(nombre).subscribe();
     this.clienteService.deleteEliminarToken(nombre).subscribe();
+    //Remueve el token de la sesion
     sessionStorage.removeItem(environment.TOKEN);
   }
 }
