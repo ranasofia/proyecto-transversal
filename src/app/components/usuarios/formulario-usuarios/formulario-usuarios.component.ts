4f99e4a446774714ee0d199e82ea0042cbd30328
Tatiana Villanueva <tatianarv14@gmail.com>
2021-05-22 23:24:18 -0500

    Interfaz de formulario crud usuarios terminada

--- src/app/components/usuarios/formulario-usuarios/formulario-usuarios.component.ts
+++ src/app/components/usuarios/formulario-usuarios/formulario-usuarios.component.ts
@@ -1,3 +1,5 @@
+import { ValidacionesPropias } from 'src/app/_model/utilidades/ValidacionesPropias';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioUsuariosComponent implements OnInit {
 
+  /**
+ * Variable que indica el formulario del usuario
+ */
+  userForm: FormGroup;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.userForm = this.createFormGroup();
   }
 
+  /**
+   * Método que se encarga de configurar las validaciones del formulario de usuario
+   * @returns grupoFormulario
+   */
+  createFormGroup() {
+    return new FormGroup({
+
+      id: new FormControl(),
+      
+      nombre: new FormControl('', [
+        Validators.required
+      ]),
+      apellido: new FormControl('', [
+        Validators.required
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      username: new FormControl('', [
+        Validators.required
+      ]),
+      fechaNacimiento: new FormControl('', [
+        Validators.required,
+        ValidacionesPropias.edadValida
+      ]),
+      identificacion: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      celular: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7)
+      ]),
+      direccion: new FormControl('', [
+        Validators.required
+      ]),
+    });
+  }
+
+  /**
+ * 
+ * @param event objeto que posee los datos del evento que ejecutó el envío del formulario
+ */
+  aceptar(event: Event): any {
+    event.preventDefault();
+  }
 }
